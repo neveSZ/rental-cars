24c3c3337dd6f7584c5e5181f9869690dbd745ef
neveSZ <vnsilva2000@gmail.com>
2020-08-18 18:04:13 -0300

    Cadastrar uma categoria de carro

--- app/controllers/car_categories_controller.rb
+++ app/controllers/car_categories_controller.rb
@@ -6,4 +6,17 @@ class CarCategoriesController < ApplicationController
   def show
     @car_category = CarCategory.find(params[:id])
   end
+
+  def new
+    @car_category = CarCategory.new
+  end
+
+  def create
+    @car_category = CarCategory.create(car_category_params)
+    redirect_to @car_category
+  end
+
+  private def car_category_params
+    params.require(:car_category).permit(:name, :daily_rate, :car_insurance, :third_party_insurance)
+  end
 end
