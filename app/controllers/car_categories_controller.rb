57792856d94be9684e6bee889d58300d422d5158
neveSZ <vnsilva2000@gmail.com>
2020-08-18 21:46:58 -0300

    Validar dados de uma categoria de carro

--- app/controllers/car_categories_controller.rb
+++ app/controllers/car_categories_controller.rb
@@ -12,8 +12,12 @@ class CarCategoriesController < ApplicationController
   end
 
   def create
-    @car_category = CarCategory.create(car_category_params)
-    redirect_to @car_category
+    @car_category = CarCategory.new(car_category_params)
+    if @car_category.save
+      redirect_to @car_category
+    else
+      render :new
+    end
   end
 
   private def car_category_params
